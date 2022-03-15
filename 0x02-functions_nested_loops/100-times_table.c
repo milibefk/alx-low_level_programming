8b08147117edfeb1a3dc8c145213a96e948437f4
milibefk <99682095+milibefk@users.noreply.github.com>
2022-03-15 07:40:43 -0700

    fff

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,29 @@
-#include "main.h"
-/**
- * print_times_table - Prints the n times table
- *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
+/*
+ * File: 100-times_table.c
+ * Auth: Michael Ndungu Kamotho
  */
-void print_times_table(int n)
-{
-	int a, b, op;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (a = 0; a <= n; a++)
-		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
-			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-			}
-			_putchar('\n');
-		}
+#include "main.h"
+
+/**
+ * 100-times_table - function that prints the n times table, starting with 0
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+
+void times_table(int n){
+	int number = 0;
+	int by = 0;
+	int result, count;
+	for(count = 0;count < n; count++){
+	
+	   for(count = 0; count < 10; count++){
+	      result = number * by;
+	      by++;
+	      _putchar(result);
+	   }
+	number++;
+	_putchar('\n')
 	}
 }
