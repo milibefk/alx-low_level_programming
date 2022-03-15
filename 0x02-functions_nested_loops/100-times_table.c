9acacbabc3721de1138a9d8695b43473c5a37670
milibefk <99682095+milibefk@users.noreply.github.com>
2022-03-15 08:21:10 -0700

    ss

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,10 +4,10 @@
  * @n: The multiplication table requested.
  * Return: Nothing.
  */
-
 void print_times_table(int n)
 {
 	int i, j, res;
+
 	if (!(n > 15 || n < 0))
 	{
 		for (i = 0; i <= n; i++)
