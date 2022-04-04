745b6d8c1b4756f25c54e101e5ad8423a8d17f09
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-04 11:02:15 -0700

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,80 +1,31 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> a77d10a15e5d1a12b1b493c9797b66598eaab382
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
 /**
- * wrdcnt - counts the number of words in a string
- * @s: string to count
+ * strtow - concatenates arguments.
+ * @str: String to be splitted.
  *
- * Return: int of number of words
- */
-int wrdcnt(char *s)
-{
-	int i, n = 0;
-
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] == ' ')
-		{
-			if (s[i + 1] != ' ' && s[i + 1] != '\0')
-				n++;
-		}
-		else if (i == 0)
-			n++;
-	}
-	n++;
-	return (n);
-}
-
-/**
- * strtow - splits a string into words
- * @str: string to split
- *
- * Return: pointer to an array of strings
+ * Return: a pointer to array of String.
  */
 char **strtow(char *str)
 {
-	int i, j, k, l, n = 0, wc = 0;
-	char **w;
+char *array = NULL;
+unsigned int i = 0, j = 0, k;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	n = wrdcnt(str);
-	if (n == 1)
-		return (NULL);
-	w = (char **)malloc(n * sizeof(char *));
-	if (w == NULL)
-		return (NULL);
-	w[n - 1] = NULL;
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-		{
-			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
-				;
-			j++;
-			w[wc] = (char *)malloc(j * sizeof(char));
-			j--;
-			if (w[wc] == NULL)
-			{
-				for (k = 0; k < wc; k++)
-					free(w[k]);
-				free(w[n - 1]);
-				free(w);
-				return (NULL);
-			}
-			for (l = 0; l < j; l++)
-				w[wc][l] = str[i + l];
-			w[wc][l] = '\0';
-			wc++;
-			i += j;
-		}
-		else
-			i++;
-	}
-	return (w);
+if (strncmp(str, "", 1) || str == NULL)
+return (NULL);
+array = malloc((i + j + 1) * sizeof(char));
+if (array == NULL)
+return (NULL);
+for (k = 0; k < i; k++)
+array[k] = str[k];
+i = k;
+for (k = 0; k < j; k++)
+{
+array[i] = str[k];
+i++;
+}
+array[i] = '\0';
+return (NULL);
 }
