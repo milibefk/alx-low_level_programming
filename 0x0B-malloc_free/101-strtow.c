68e6e7b6aa857c94041b13958fae04d822a773c5
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-04 11:08:50 -0700

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,31 +1,77 @@
-#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include "holberton.h"
+
 /**
- * strtow - concatenates arguments.
- * @str: String to be splitted.
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- * Return: a pointer to array of String.
+ * Return: number of words
+ */
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-char *array = NULL;
-unsigned int i = 0, j = 0, k;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-if (strncmp(str, "", 1) || str == NULL)
-return (NULL);
-array = malloc((i + j + 1) * sizeof(char));
-if (array == NULL)
-return (NULL);
-for (k = 0; k < i; k++)
-array[k] = str[k];
-i = k;
-for (k = 0; k < j; k++)
-{
-array[i] = str[k];
-i++;
-}
-array[i] = '\0';
-return (NULL);
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
