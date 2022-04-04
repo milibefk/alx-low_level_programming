ed840ab140ff4d7ca6ad4fe8233e93ac74115e3c
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-04 11:20:20 -0700

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,77 +1,51 @@
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
+ * argstostr - Concatenates all the arguments of the program
  *
- * Return: number of words
+ * @ac: Argument total count
+ *
+ * @av: Pointer to arguments
+ *
+ * Retunr: Pointer to concatenated string (SUCCESS) or
+ * NULL if @ac == 0 or @av == NULL (FAILURE) or
+ * NULL if if insufficient memory was available (FAILURE)
  */
-int count_word(char *s)
+
+char *argstostr(int ac, char **av)
 {
-	int flag, c, w;
+	int i, j;
+	int count = 0;
+	int t_count = 0;
+	char *result;
 
-	flag = 0;
-	w = 0;
+	if (ac == 0 || av == NULL)
+		return ('\0');
 
-	for (c = 0; s[c] != '\0'; c++)
+	for (i = 0; i < ac; i++)
 	{
-		if (s[c] == ' ')
-			flag = 0;
-		else if (flag == 0)
-		{
-			flag = 1;
-			w++;
-		}
+		for (j = 0; av[i][j] != '\0'; j++)
+			t_count++;
+
+		t_count++;
 	}
 
-	return (w);
-}
-/**
- * **strtow - splits a string into words
- * @str: string to split
- *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
- */
-char **strtow(char *str)
-{
-	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
+	result = malloc(sizeof(char) * t_count + 1);
 
-	while (*(str + len))
-		len++;
-	words = count_word(str);
-	if (words == 0)
-		return (NULL);
-
-	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-	if (matrix == NULL)
-		return (NULL);
-
-	for (i = 0; i <= len; i++)
+	if (result == NULL)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
-		{
-			if (c)
-			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
-					return (NULL);
-				while (start < end)
-					*tmp++ = str[start++];
-				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
-			}
-		}
-		else if (c++ == 0)
-			start = i;
+		return ('\0');
 	}
 
-	matrix[k] = NULL;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			result[count++] = av[i][j];
+		}
+		result[count++] = '\n';
+	}
 
-	return (matrix);
+	result[t_count] = '\0';
+	return (result);
 }
