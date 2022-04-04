962e9da99d51ec4e407609576a46aec4b5830383
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-04 00:51:01 -0700

    adas

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,52 +1,51 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
-* char *argstostr - concatenates all the arguments of a program.
-* @ac: arguments counter
-* @av: argument matrix
-* Return: a pointer to a new string, or NULL if it fails.
-* also return NULL  if ac = 0 or av = NULL
-**/
+ * argstostr - Concatenates all the arguments of the program
+ *
+ * @ac: Argument total count
+ *
+ * @av: Pointer to arguments
+ *
+ * Retunr: Pointer to concatenated string (SUCCESS) or
+ * NULL if @ac == 0 or @av == NULL (FAILURE) or
+ * NULL if if insufficient memory was available (FAILURE)
+ */
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, count;
-	char *s;
-
-	count = 0;
+	int i, j;
+	int count = 0;
+	int t_count = 0;
+	char *result;
 
 	if (ac == 0 || av == NULL)
-	{
-		return (NULL);
-	}
+		return ('\0');
+
 	for (i = 0; i < ac; i++)
 	{
-		j = 0;
-		while (av[i][j])
-		{
-			j++;
-			count++;
-		}
-		count++;
+		for (j = 0; av[i][j] != '\0'; j++)
+			t_count++;
+
+		t_count++;
 	}
-	s = malloc(sizeof(char) * count + 1);
-	if (s != NULL)
+
+	result = malloc(sizeof(char) * t_count + 1);
+
+	if (result == NULL)
 	{
-		count = 0;
-		for (i = 0; i < ac; i++)
-		{
-			j = 0;
-			while (av[i][j])
-			{
-				s[count] = av[i][j];
-				count++;
-				j++;
-			}
-		s[count] = '\n';
-		count++;
-		}
-	return (s);
+		return ('\0');
 	}
-	return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			result[count++] = av[i][j];
+		}
+		result[count++] = '\n';
+	}
+
+	result[t_count] = '\0';
+	return (result);
 }
