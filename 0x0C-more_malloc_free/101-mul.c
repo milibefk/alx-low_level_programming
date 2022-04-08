19d160de01c49d68284d4dc65fe3e323ec3d4817
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-08 10:35:23 -0700

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,6 +8,12 @@
  *
  * Return: no return.
  */
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 void _is_zero(char *argv[])
 {
 	int i, isn1 = 1, isn2 = 1;
