882eb3933890a83bdadfa39ea76eb219a5f91df5
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-08 12:26:15 -0700

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -61,8 +61,7 @@ char *_initialize_array(char *ar, int lar)
 int _checknum(char *argv[], int n)
 {
 	int ln;
-	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
+	for (ln = 0; argv[n][ln]; ln++)if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
