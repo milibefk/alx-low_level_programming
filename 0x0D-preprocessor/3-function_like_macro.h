7afc0a5cc6a9e1b01ce5359fa620a0104e25bd8e
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-11 02:53:56 -0700

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -6,8 +6,9 @@
  * Auth: mili
  * Desc: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
+ *#define ABS(x) ((x) < 0 ? (-x) : (x))
  */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define abs(x) ((x)<0 ? (-x) : (x))
+#define abs(x) ((x)<0 ? -(x) : (x))
 
 #endif
