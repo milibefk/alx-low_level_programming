9ddb8bd8713ec32b8cdb84b8cca709aceb95efd4
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-13 13:47:16 -0700

    cgf

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,16 @@
-#include "fun_main.h"
+#include "function_pointers.h"
 #include <stdlib.h>
+
 /**
- * print_name - prints the name
- * @name: name
- * @f: function
+ * print_name - prints a name
+ * @name: string
+ * @f: function pointer
+ * Return: void
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (print_name == NULL || f == NULL)
-		f(print_name);
-	return;
+	if (f == NULL)
+		return;
+	f(name);
 }
