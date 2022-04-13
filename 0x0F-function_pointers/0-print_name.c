3e93ca9f6378e937aac1c559ad5aa8b5952393ad
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-13 11:27:10 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,13 @@
-/*hhf
- * File: 0-print_name.c
- * Auth:mili
- */
-
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * print_name - Prints a name.
- * @name: The name to be printed.
- * @f: A pointer to a function that prints a name.
+ * print_name - print name
+ * @name : string
+ * @f: function
+ * Return: void
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
