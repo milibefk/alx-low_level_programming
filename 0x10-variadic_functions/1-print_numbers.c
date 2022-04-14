61ec18f6792642e230bdf16dd01cc0cb4e09bda2
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-14 01:50:41 -0700

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 
 /**
+ * Auth: Miliyon Befkadu
  * print_numbers - prints numbers
  * @separator: pointer to the string to be printed between numbers
  * @n: number of integers passed to the function
